17b98363ada2a58970e8da5cb52babd38324842c
Matheus Heisler <mhheisler@gmail.com>
2020-12-02 10:06:00 -0300

    first

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+import SignOutButton from '../SignOut';
+import { AuthUserContext } from '../Session';
+
+const Navigation = () => (
+    <div className="Navigation">
+        <AuthUserContext.Consumer>
+            {authUser =>
+                authUser ? <NavigationAuth /> : <NavigationNonAuth />
+            }
+        </AuthUserContext.Consumer>
+    </div>
+);
+
+const NavigationAuth = () => (
+    <div>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+
+        <Link to={ROUTES.HOME}>Home</Link>
+
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+
+        <SignOutButton />
+    </div>
+);
+
+const NavigationNonAuth = () => (
+    <div>
+            <Link to={ROUTES.LANDING}>Landing</Link>
+
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </div>
+);
+
+export default Navigation;
